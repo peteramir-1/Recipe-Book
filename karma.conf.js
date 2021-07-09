7b6fed655a4e29f3143951e7868c78c073f5ddc1
Peter Amir <peteramir5522@gmail.com>
2021-07-09 06:19:41 +0200

    upgrade to Angular 12.1.1

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/CourseProject'),
+      dir: require('path').join(__dirname, './coverage/recipe-book-update'),
       subdir: '.',
       reporters: [
         { type: 'html' },
